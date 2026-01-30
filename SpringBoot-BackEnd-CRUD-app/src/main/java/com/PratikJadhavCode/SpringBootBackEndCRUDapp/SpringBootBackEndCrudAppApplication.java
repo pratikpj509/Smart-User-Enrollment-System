aef2aab8998c13e6474c572774dd855fbf18edc8
pratikpj509 <pratikpj509@gmail.com>
2026-01-30 17:13:32 +0530

    Added

---
+++ SpringBoot-BackEnd-CRUD-app/src/main/java/com/PratikJadhavCode/SpringBootBackEndCRUDapp/SpringBootBackEndCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.PratikJadhavCode.SpringBootBackEndCRUDapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackEndCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackEndCrudAppApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-BackEnd-CRUD-app/src/main/java/com/PratikJadhavCode/SpringBootBackEndCRUDapp/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.PratikJadhavCode.SpringBootBackEndCRUDapp.exception;
+
+public class UserNotFoundException extends RuntimeException{
+	
+	public UserNotFoundException(Long id) {
+		super("Could not found the user with id "+id);
+	}
+
+}
